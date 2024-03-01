d37a16c8795f5333ebf789f53846d5e181a9660d
Fpierr <fanuelp9@gmail.com>
2024-03-01 16:46:21 +0100

    a program called console.py that contains the entry point of the command interpreter

---
+++ console.py
@@ -0,0 +1,23 @@
+#!/usr/bin/python3
+"""Command interpreter for the AirBnB clone project"""
+
+import cmd
+
+
+
+class HBNBCommand(cmd.Cmd):
+    """Command interpreter class"""
+
+    prompt = "(hbnb) "
+
+    def do_quit(self, arg):
+        """Quit command to exit the program"""
+        return True
+
+    def do_EOF(self, arg):
+        """EOF command to exit the programm"""
+        print("")
+        return True
+
+if __name__ == '__main__':
+    HBNBCommand().cmdloop()
