5aa137529efca4a6fb63efc5775b4bd210e1cdec
Fpierr <fanuelp9@gmail.com>
2024-03-05 12:42:29 +0100

    update base model

--- models/base_model.py
+++ models/base_model.py
@@ -17,15 +17,15 @@ class BaseModel:
                     value = datetime.fromisoformat(value)
                 if key != '__class__':
                     setattr(self, key, value)
-
-        self.id = str(uuid.uuid4())
-        self.created_at = datetime.now()
-        self.updated_at = datetime.now()
+        else:
+            self.id = str(uuid.uuid4())
+            self.created_at = datetime.now()
+            self.updated_at = datetime.now()
 
     def __str__(self):
         """Return the string representation of BaseModel."""
         return "[{}] ({}) {}".format(
-            self.__class__.__name__, self.id, self.__dict__)
+            type(self).__name__, self.id, self.__dict__)
 
     def save(self):
         """update the instanse update attribute and call save on storage"""
@@ -35,7 +35,7 @@ class BaseModel:
     def to_dict(self):
         """Create a dictionary that content the values of the instance"""
         model_dict = self.__dict__.copy()
-        model_dict['__class__'] = self.__class__.__name__
+        model_dict['__class__'] = type(self).__name__
         model_dict['created_at'] = self.created_at.isoformat()
         model_dict['updated_at'] = self.updated_at.isoformat()
         return model_dict
