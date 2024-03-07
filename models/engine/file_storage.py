d21821101c1d3a1b4970ec6c9271f5109a00d15b
Fpierr <fanuelp9@gmail.com>
2024-03-07 08:31:17 +0100

    file storage base is valid in local

--- models/engine/file_storage.py
+++ models/engine/file_storage.py
@@ -4,16 +4,15 @@
 import json
 from models.base_model import BaseModel
 from datetime import datetime
+import os
 
 
 class FileStorage:
     """Manage serialization/deserialization of objects to/from json"""
 
     # path to the JSON file and dictionary to store object
-    def __init__(self):
-        """Initialize FileStorage instance"""
-        self.__file_path = "file.json"
-        self.__objects = {}
+    __file_path = "file.json"
+    __objects = {}
 
     def all(self, cls=None):
         """return the dictionary object or filtered by cls"""
@@ -34,22 +33,28 @@ class FileStorage:
     def save(self):
         """serialize __objects to the JSON file"""
         obj_dict = {}
-        for key, value in self.__objects.items():
+        for key, value in FileStorage.__objects.items():
             obj_dict[key] = value.to_dict()
-        with open(self.__file_path, 'w', encoding='utf-8') as file:
+
+        with open(FileStorage.__file_path, 'w') as file:
             json.dump(obj_dict, file)
 
     def reload(self):
         """deserialize the JSON file to __objects"""
         try:
-            with open(self.__file_path, 'r', encoding='utf-8') as file:
+            with open(FileStorage.__file_path, 'r') as file:
                 obj_dict = json.load(file)
                 for key, value in obj_dict.items():
-                    class_name, obj_id = key.split('.')
+                    class_name, obj_id = key.rsplit('.', 1)
                     value['created_at'] = datetime.strptime(
                         value['created_at'], "%Y-%m-%dT%H:%M:%S.%f")
                     value['updated_at'] = datetime.strptime(
                         value['updated_at'], "%Y-%m-%dT%H:%M:%S.%f")
+                    #convert value in strins
+                    for k, v in value.items():
+                        if isinstance(v, datetime):
+                            value[k] = v.isoformat()
+
                     obj_class = globals()[class_name]
                     obj_instance = obj_class(**value)
                     self.__objects[key] = obj_instance
