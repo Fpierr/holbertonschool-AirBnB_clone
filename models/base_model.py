8ada216c563b76dd68af0315d0f5ab9d7d8e8fe3
Fpierr <fanuelp9@gmail.com>
2024-03-07 15:42:24 +0100

    BM

--- models/base_model.py
+++ models/base_model.py
@@ -28,7 +28,6 @@ class BaseModel:
         else:
             self.id = str(uuid.uuid4())
             self.created_at = datetime.now()
-            self.updated_at = datetime.now()
 
     def __str__(self):
         """Return the string representation of BaseModel."""
